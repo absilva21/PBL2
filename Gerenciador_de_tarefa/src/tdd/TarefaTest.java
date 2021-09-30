78d319f406ba212d3dc4e4a0e1bf37e8e78002f2
alisson <alisson@alisson-All-Series>
2021-09-30 17:21:29 -0300

    Construção de teste com Junit 4

---
+++ Gerenciador_de_tarefa/src/tdd/TarefaTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package tdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author alisson
+ *
+ */
+public class TarefaTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
