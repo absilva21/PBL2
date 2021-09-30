f49ee2bd54ff0c1de704eda0f5fe631c8ef2acb5
alisson <alisson@alisson-All-Series>
2021-09-30 09:01:54 -0300

    # WARNING: head commit changed in the meantime
    Classe tarefa

---
+++ Gerenciador_de_tarefa/src/system/Tarefa.java
@@ -0,0 +1,15 @@
+package system;
+
+import java.util.Observable;
+import java.util.Observer;
+
+@SuppressWarnings("deprecation")
+public class Tarefa implements Observer {
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
