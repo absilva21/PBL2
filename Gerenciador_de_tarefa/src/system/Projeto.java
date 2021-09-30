45e921d0221d9f53d67719af1ae26dbb7018ac55
alisson <alisson@alisson-All-Series>
2021-09-30 20:30:43 -0300

    Criando Classe Projeto

--- Gerenciador_de_tarefa/src/system/Projeto.java
+++ Gerenciador_de_tarefa/src/system/Projeto.java
@@ -1,5 +1,27 @@
 package system;
-
+import colections.LinkList;
 public class Projeto {
-
+	private LinkList<Tarefa> tarefas;
+	private String descricao;
+	private String titulo;
+	
+	public LinkList<Tarefa> getTarefas() {
+		return tarefas;
+	}
+	public void setTarefas(LinkList<Tarefa> tarefas) {
+		this.tarefas = tarefas;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
 }
