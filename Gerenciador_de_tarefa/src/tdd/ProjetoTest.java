a3dd0b22a8336a78c57ed02a395cd4fc0bd6500b
alisson <alisson@alisson-All-Series>
2021-10-06 10:17:28 -0300

    criando mais Teste

--- Gerenciador_de_tarefa/src/tdd/ProjetoTest.java
+++ Gerenciador_de_tarefa/src/tdd/ProjetoTest.java
@@ -8,11 +8,40 @@ import org.junit.Test;
 
 public class ProjetoTest {
 	Projeto a = Projeto("Projeto x","criar algo");
+	Tarefa x1 = new Tarefa("fazer classe x","implentar a classe x", new Date()); 
+
+	/**
+	 * Testa se a tarefa foi adicionada com sucesso
+	 */
 	@Test
 	public void  addTarefaTest() {
-		Tarefa x1 = new Tarefa("fazer classe x","implentar a classe x", new Date()); 
 		a.addTarefa(x1);
 		assertEquals(x1,a.getTarefas().getInicio().getData());
+	}	
+	
+	/**
+	 * testa se a tarefa foi excluida com sucesso
+	 */
+	@Test
+	public void excluirTarefaTest() {
+		a.excluirTarefa("fazer classe x");
+		assertTrue(a.getTarefas().isEmpty());
+	}
+	/**
+	 * Testa a alteração dos dados de uma tarefa
+	 */
+	@Test
+	public void editarTarefaTest() {
+		a.editarTarefa("fazer classe x","fsd vlsdr x","implementar a classe x", 121425152);
+		AssertEquals(a.buscarPeloTitulo("fsd vlsdr x"),x1);
+	}
+	
+	/**
+	 * Testa a busca pelo titulo da tarefa
+	 */
+	@Test
+	public void buscarPeloTituloTest() {
+		AssetEquals(a.buscarPeloTitulo("fazer classe x"),x1);
 	}
 
 }
--- Gerenciador_de_tarefa/src/tdd/TarefaTest.java
+++ Gerenciador_de_tarefa/src/tdd/TarefaTest.java
@@ -13,29 +13,41 @@ import system.Tarefa;
  */
 public class TarefaTest {
 	
+	
 	Tarefa a = new Tarefa("Fazer classe 'x'","Criar a classe x com antecedencia",new Date());
 	/**
 	 * @throws java.lang.Exception
 	 */
 
+	/**
+	 *  Testa se a tarefa foi marcada como pendete
+	 */
 	@Test
 	public void marcarPendenteTeste() {
 		assertEquals("Pendente",a.getSituacao());
 	}
 	
+	/***
+	 * Testa se a tarefa foi marca como "em execução"
+	 */
 	@Test
 	public void marcarEmExecTest() {
 		a.marcarExecucao();
 		assertEquals("Em execução",a.getSituacao());
 	}
 	
+	/***
+	 * Testa se a tarefa foi marcada como finalida
+	 */
 	@Test
 	public void marcarFinalizadaTest() {
 		a.marcarFinalizada();
 		assertEquals("Finalizada",a.getSituacao());
 		
 	}
-	
+	/**
+	 * testa se a tarefa está pedendete após atraso
+	 */
 	@Test
 	public void updateTest() {
 		SystemHoraData sys = SystemHoraData();
