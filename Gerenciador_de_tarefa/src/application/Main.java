c568a849463681228628f9555c14170d3191ec06
alisson <alisson@alisson-All-Series>
2021-10-18 20:31:15 -0300

    UI

--- Gerenciador_de_tarefa/src/application/Main.java
+++ Gerenciador_de_tarefa/src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -9,12 +11,17 @@ import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	static Stage aux;
+	static AnchorPane form;
+	static AnchorPane home;
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws IOException {
 		aux = primaryStage;
+		form = (AnchorPane)FXMLLoader.load(getClass().getResource("formularioProjeto.fxml"));
+		home = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+		
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,600,400);
+			
+			Scene scene = new Scene(home,600,400);
 	
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
--- Gerenciador_de_tarefa/src/application/SampleController.java
+++ Gerenciador_de_tarefa/src/application/SampleController.java
@@ -8,7 +8,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import system.Projeto;
 import system.SystemGerenciador;
 
 
@@ -20,34 +24,65 @@ public class SampleController {
     private Button add;
     
     @FXML
-    private Label title;
+    private TextField title;
     
     @FXML
-    private Label descri;
+    private TextArea descri;
     
     
-
     public SampleController() {
     	sys = new SystemGerenciador();
     }
     @FXML
     void addOnAction(ActionEvent event) throws IOException {
     	
-    	AnchorPane form = (AnchorPane)FXMLLoader.load(getClass().getResource("formularioProjeto.fxml"));
-    	Scene sceneform = new Scene(form,600,400);
+    
+    	Scene sceneform = new Scene(Main.form,600,400);
     	Main.aux.setScene(sceneform);
      
     }
     
     @FXML
     void backOnAction(ActionEvent event) throws IOException {
-    	AnchorPane form = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-    	Scene sceneform = new Scene(form,600,400);
+    	
+    	Scene sceneform = new Scene(Main.home,600,400);
     	Main.aux.setScene(sceneform);
     }
     
     @FXML
     void criaProjetoOnAction(ActionEvent event)throws IOException{
     	sys.criarProjeto(title.getText(),descri.getText());
+    	carregarProjetos();
+    	
+    }
+    
+    public void carregarProjetos(){
+    	sys.getProjetos().resetIndex();
+    	Projeto a = sys.getProjetos().next();
+    	
+    	if(a!=null) {
+    		VBox pro = new VBox();
+	    	Label title = new Label(a.getTitulo());
+	    	Label descri = new Label(a.getDescricao());
+	    	Button deta = new Button("ver tarefas");
+	    	pro.getChildren().addAll(title,descri,deta);
+	    	Main.home.getChildren().addAll(pro);
+    	}
+    	
+    	
+    	while(sys.getProjetos().getIndex()!=null) {
+    		if(a!=null) {
+        		VBox pro = new VBox();
+    	    	Label title = new Label(a.getTitulo());
+    	    	Label descri = new Label(a.getDescricao());
+    	    	Button deta = new Button("ver tarefas");
+    	    	pro.getChildren().addAll(title,descri,deta);
+    	    	Main.home.getChildren().addAll(pro);
+    	    	a = sys.getProjetos().next();
+        	}else {
+        		break;
+        	}
+    	}
+    	
     }
 }
