005cb1ff8801b28e7c35500c650e6a8dc027070e
alisson <alisson@alisson-All-Series>
2021-10-18 09:14:44 -0300

    finalização os testes de Systemgerenciador

--- Gerenciador_de_tarefa/src/tdd/SystemGerenciadorTest.java
+++ Gerenciador_de_tarefa/src/tdd/SystemGerenciadorTest.java
@@ -8,7 +8,9 @@ import system.Projeto;
 public class SystemGerenciadorTest {
 	
 	SystemGerenciador sys;
-	
+	/**
+	 * testa a criação de um Projeto
+	 */
 	@Test
 	public void criarProjetoTest() {
 		sys = new SystemGerenciador();
@@ -16,5 +18,38 @@ public class SystemGerenciadorTest {
 		Projeto a = (Projeto)sys.getProjetos().getInicio().getData();
 		assertEquals(a,sys.buscarPeloTitulo("projeto x"));
 	}
+	
+	/**
+	 * Testa o método de busca 
+	 */
+	@Test
+	public void buscarPeloTituloTest() {
+		sys = new SystemGerenciador();
+		sys.criarProjeto("projeto x", "desenvolvimento do projeto x");
+		assertEquals("projeto x", sys.buscarPeloTitulo("projeto x").getTitulo());
+	}
+	/**
+	 * testa a edição dos parâmetros de um Projeto
+	 */
+	@Test
+	public void editarProjetoTest() {
+		sys = new SystemGerenciador();
+		sys.criarProjeto("projeto x", "desenvolvimento do projeto x");
+		sys.editarProjeto("projeto x", "projetox","desenvolvimento do projeto x");
+		assertEquals(sys.getProjetos().getInicio().getData(),sys.buscarPeloTitulo("projetox"));
+	}
+	
+	/**
+	 * testa a exclusão  de um Projeto da Lista
+	 */
+	@Test
+	public void apagarProjetoTest() {
+		sys = new SystemGerenciador();
+		sys.criarProjeto("projeto x", "desenvolvimento do projeto x");
+		assertEquals("projeto x", sys.buscarPeloTitulo("projeto x").getTitulo());
+		sys.apagarProjeto("projeto x");
+		assertTrue(sys.getProjetos().isEmpty());
+	}
+	
 
 }
