e811c5e0fde843e12bbfc02a13a12094de8fefa8
alisson <alisson@alisson-All-Series>
2021-10-18 11:53:28 -0300

    densenvolvimento da UI

--- Gerenciador_de_tarefa/src/application/SampleController.java
+++ Gerenciador_de_tarefa/src/application/SampleController.java
@@ -1,5 +1,18 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class SampleController {
 	
+	
+	
+    @FXML
+    private Button add;
+
+    @FXML
+    void addOnAction(ActionEvent event) {
+    	
+    }
 }
