001c8667b2e78bd2d1870722a54aef2e47045cda
alisson <alisson@alisson-All-Series>
2021-10-18 16:47:41 -0300

    UI

--- Gerenciador_de_tarefa/src/application/Main.java
+++ Gerenciador_de_tarefa/src/application/Main.java
@@ -8,12 +8,14 @@ import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	static Stage aux;
 	@Override
 	public void start(Stage primaryStage) {
+		aux = primaryStage;
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
-		
+			Scene scene = new Scene(root,600,400);
+	
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -21,8 +23,6 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
-	
 	
 	
 	
--- Gerenciador_de_tarefa/src/application/SampleController.java
+++ Gerenciador_de_tarefa/src/application/SampleController.java
@@ -1,8 +1,13 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 public class SampleController {
 	
@@ -12,7 +17,11 @@ public class SampleController {
     private Button add;
 
     @FXML
-    void addOnAction(ActionEvent event) {
+    void addOnAction(ActionEvent event) throws IOException {
     	
+    	AnchorPane form = (AnchorPane)FXMLLoader.load(getClass().getResource("formularioProjeto.fxml"));
+    	Scene sceneform = new Scene(form,600,400);
+    	Main.aux.setScene(sceneform);
+     
     }
 }
